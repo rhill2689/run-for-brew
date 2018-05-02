604a7ad5bda77f0d9397ea0fac3624ccc72af0c8
ryanhill <rhill@ippon.fr>
2018-05-02 17:42:12 -0400

    Initial application generated by JHipster-4.14.3

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,89 @@
+import './vendor.ts';
+
+import { NgModule, Injector } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Ng2Webstorage, LocalStorageService, SessionStorageService  } from 'ngx-webstorage';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
+import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
+import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
+import { RfbSharedModule, UserRouteAccessService } from './shared';
+import { RfbAppRoutingModule} from './app-routing.module';
+import { RfbHomeModule } from './home/home.module';
+import { RfbAdminModule } from './admin/admin.module';
+import { RfbAccountModule } from './account/account.module';
+import { RfbEntityModule } from './entities/entity.module';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        RfbAppRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        RfbSharedModule,
+        RfbHomeModule,
+        RfbAdminModule,
+        RfbAccountModule,
+        RfbEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        PaginationConfig,
+        UserRouteAccessService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+            deps: [
+                LocalStorageService,
+                SessionStorageService
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthExpiredInterceptor,
+            multi: true,
+            deps: [
+                Injector
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorHandlerInterceptor,
+            multi: true,
+            deps: [
+                JhiEventManager
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: NotificationInterceptor,
+            multi: true,
+            deps: [
+                Injector
+            ]
+        }
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class RfbAppModule {}
