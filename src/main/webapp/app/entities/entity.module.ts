a3ccc15d13cb51cf49348a854eb62f745c101e43
ryanhill <rhill@ippon.fr>
2018-05-03 12:19:25 -0400

    first commit

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,17 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { RfbRfbLocationModule } from './rfb-location/rfb-location.module';
+import { RfbRfbEventModule } from './rfb-event/rfb-event.module';
+import { RfbRfbEventAttendanceModule } from './rfb-event-attendance/rfb-event-attendance.module';
+import { RfbRfbUserModule } from './rfb-user/rfb-user.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        RfbRfbLocationModule,
+        RfbRfbEventModule,
+        RfbRfbEventAttendanceModule,
+        RfbRfbUserModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
